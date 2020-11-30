1cfac8031d545eeb6f6e88cfac8f8b48e7cf28fb
bvarv <qq066900>
2020-11-30 17:53:21 +0200

    8

--- app/app/schema.py
+++ app/app/schema.py
@@ -8,6 +8,8 @@ class Query(users.schema.Query, tracks.schema.Query, graphene.ObjectType):
     pass
 
 class Mutation(users.schema.Mutation, tracks.schema.Mutation, graphene.ObjectType):
-    pass
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
