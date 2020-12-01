0dc1d1b5b7e5ca4377667a6bbd13d8fc747d8441
bvarv <qq066900>
2020-12-01 11:02:35 +0200

    backend finished

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 from graphql import GraphQLError
-
+from django.db.models import Q
 from users.schema import UserType
 from .models import Track, Like
 
@@ -15,10 +15,18 @@ class LikeType(DjangoObjectType):
         model = Like
 
 class Query(graphene.ObjectType):
-    tracks = graphene.List(TrackType)
+    tracks = graphene.List(TrackType, search=graphene.String())
     likes = graphene.List(LikeType)
 
-    def resolve_tracks(self, info):
+    def resolve_tracks(self, info, search):
+        if search:
+            filter = (
+                    Q(title__icontains=search) |
+                    Q(description__icontains=search) |
+                    Q(url__icontains=search) |
+                    Q(posted_by__username__icontains=search)
+            )
+            return Track.objects.filter(filter)
         return Track.objects.all()
 
     def resolve_likes(self, info):
