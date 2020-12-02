a2240925e9880c344b6b49c9e9a8c9dea83bac8a
bvarv <qq066900>
2020-12-02 12:35:26 +0200

    added router

--- react-tracks-clients/src/Root.js
+++ react-tracks-clients/src/Root.js
@@ -2,6 +2,11 @@ import React from "react";
 import withRoot from "./withRoot";
 import { Query } from "react-apollo";
 import { gql } from 'apollo-boost';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import App from "./pages/App";
+import Profile from "./pages/Profile";
+import Header from "./components/Shared/Header";
+
 
 const Root = () => (
     <Query query={ME_QUERY}>
@@ -9,7 +14,17 @@ const Root = () => (
             console.log(111, data)
             if (loading) return  <div>Loading..</div>
             if (error) return  <div> error..</div>
-            return <div>{JSON.stringify(data)}</div>
+            return (
+                <>
+                    <Header />
+                    <Router>
+                        <Switch>
+                            <Route exact path='/' component={App}/>
+                            <Route path='/profile/:id' component={Profile}/>
+                        </Switch>
+                    </Router>
+                </>
+            )
         }}
     </Query>
 );
