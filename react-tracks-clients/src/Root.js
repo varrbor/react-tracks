2de1ac19919f0383f05e39da1b3a2b26428ebb26
bvarv <qq066900>
2020-12-02 16:26:09 +0200

    added header

--- react-tracks-clients/src/Root.js
+++ react-tracks-clients/src/Root.js
@@ -1,32 +1,37 @@
 import React from "react";
-import withRoot from "./withRoot";
 import { Query } from "react-apollo";
-import { gql } from 'apollo-boost';
+import { gql } from "apollo-boost";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import withRoot from "./withRoot";
 import App from "./pages/App";
 import Profile from "./pages/Profile";
 import Header from "./components/Shared/Header";
+import Loading from "./components/Shared/Loading";
+import Error from "./components/Shared/Error";
 
+export const UserContext = React.createContext();
 
 const Root = () => (
-    <Query query={ME_QUERY}>
-        {({data, loading, error}) => {
-            console.log(111, data)
-            if (loading) return  <div>Loading..</div>
-            if (error) return  <div> error..</div>
-            return (
-                <>
-                    <Header />
-                    <Router>
-                        <Switch>
-                            <Route exact path='/' component={App}/>
-                            <Route path='/profile/:id' component={Profile}/>
-                        </Switch>
-                    </Router>
-                </>
-            )
-        }}
-    </Query>
+  <Query query={ME_QUERY} fetchPolicy="cache-and-network">
+    {({ data, loading, error }) => {
+      if (loading) return <Loading />;
+      if (error) return <Error error={error} />;
+      const currentUser = data.me;
+
+      return (
+        <Router>
+          <UserContext.Provider value={currentUser}>
+            <Header currentUser={currentUser} />
+            <Switch>
+              <Route exact path="/" component={App} />
+              <Route path="/profile/:id" component={Profile} />
+            </Switch>
+          </UserContext.Provider>
+        </Router>
+      );
+    }}
+  </Query>
 );
 
 export const ME_QUERY = gql`
@@ -43,15 +48,5 @@ export const ME_QUERY = gql`
     }
   }
 `;
-
-// const GET_TRACKS_QUERY = gql`
-//     {
-//       tracks(search:"") {
-//         id
-//         title
-//         url
-//       }
-//     }
-// `
 
 export default withRoot(Root);
